a59cf33fa8eef38fa8c15b81e5c9373b3714efc6
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2020-08-13 20:38:59 +0200

    I'm so freaking good

--- src/main/java/RegistryHelper.java
+++ src/main/java/RegistryHelper.java
@@ -1,4 +1,5 @@
 import com.tfc.flame.FlameConfig;
+import entries.FlameAPI.Main;
 
 import java.io.File;
 import java.io.InputStream;
@@ -9,10 +10,16 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class RegistryHelper {
-	private static final String[] blocks = new String[]{
+	private static final String[] blocks_13 = new String[]{
 			"cobblestone",
 			"air",
 			"grass_block",
+			"dirt"
+	};
+	private static final String[] blocks_12 = new String[]{
+			"cobblestone",
+			"air",
+			"grass",
 			"dirt"
 	};
 	private static final String[] items = new String[]{
@@ -26,6 +33,11 @@ public class RegistryHelper {
 		try {
 			JarFile file = new JarFile(versionDir);
 			HashMap<String, String> registries = new HashMap<>();
+			String mcVersion = Main.getVersion().substring(0, Main.getVersion().indexOf("-"));
+			String mcMiddleVersion = mcVersion.substring(2);
+			mcMiddleVersion = mcMiddleVersion.substring(0, mcMiddleVersion.indexOf("."));
+			FlameConfig.field.append(mcMiddleVersion + "\n");
+			String[] version_blocks = Integer.parseInt(mcMiddleVersion) > 12 ? blocks_13 : blocks_12;
 			try {
 				for (Iterator<JarEntry> it = file.stream().iterator(); it.hasNext(); ) {
 					JarEntry entry = it.next();
@@ -42,18 +54,25 @@ public class RegistryHelper {
 								for (String s : items)
 									if (!itemChecks.containsKey(s) && s1.contains(s))
 										itemChecks.put(s, true);
-								for (String s : blocks)
-									if (!blockChecks.containsKey(s) && s1.contains(s)) blockChecks.put(s, true);
+								for (String s : version_blocks)
+									if (!blockChecks.containsKey(s) && s1.contains(s))
+										blockChecks.put(s, true);
 							}
-							if (containsUnmodifiableIterator) {
-								if (blockChecks.size() == (blocks.length)) {
+							if (Integer.parseInt(mcMiddleVersion) > 12) {
+								if (containsUnmodifiableIterator) {
+									if (blockChecks.size() == (version_blocks.length)) {
+										registries.put("minecraft:blocks", entry.getName());
+										FlameConfig.field.append("block registry class:" + entry.getName() + "\n");
+									}
+								} else if (itemChecks.size() == (items.length)) {
+									registries.put("minecraft:items", entry.getName());
+									FlameConfig.field.append("item registry class:" + entry.getName() + "\n");
+								}
+							} else {
+								if (blockChecks.size() == (version_blocks.length)) {
 									registries.put("minecraft:blocks", entry.getName());
 									FlameConfig.field.append("block registry class:" + entry.getName() + "\n");
 								}
-								FlameConfig.field.append("Blocks Registries:" + registries.size() + "\n");
-							} else {
-								if (!itemChecks.isEmpty())
-									FlameConfig.field.append("itemChecks: " + itemChecks + "\n");
 								if (itemChecks.size() == (items.length)) {
 									registries.put("minecraft:items", entry.getName());
 									FlameConfig.field.append("item registry class:" + entry.getName() + "\n");
