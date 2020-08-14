d1ff4122b7a18bb37cba350d27372edfe2fa7b32
LorenzoPapi <42911751+LorenzoPapi@users.noreply.github.com>
2020-08-14 16:40:30 +0200

    refactore GameFormat and start of 1.7 ench search

--- src/main/java/RegistryHelper.java
+++ src/main/java/RegistryHelper.java
@@ -56,11 +56,14 @@ public class RegistryHelper {
 			"jukebox",
 			"dispenser"
 	};
-	private static final String[] enchantments = new String[]{
+	private static final String[] enchantments_12 = new String[]{
 			"protection",
 			"thorns",
 			"efficiency",
 			"sharpness"
+	};
+	private static final int[] enchantments_11 = new int[] {
+			0,1,2,3,4,5,6,7,20
 	};
 	
 	public static HashMap<String, String> findRegistryClass(File versionDir) {
@@ -78,6 +81,7 @@ public class RegistryHelper {
 			String[] version_blocks = flagGreaterThan12 ? blocks_13 : blocks_12;
 			String[] version_entities = flagLessThan11 ? entities_11 : entities_12;
 			String[] version_tileEntities = flagLessThan11 ? tileEntities_11 : tileEntities_12;
+
 			try {
 				for (Iterator<JarEntry> it = file.stream().iterator(); it.hasNext(); ) {
 					JarEntry entry = it.next();
@@ -89,7 +93,8 @@ public class RegistryHelper {
 							HashMap<String, Boolean> itemChecks = new HashMap<>();
 							HashMap<String, Boolean> tileEntitiesChecks = new HashMap<>();
 							HashMap<String, Boolean> entityChecks = new HashMap<>();
-							HashMap<String, Boolean> enchantmentChecks = new HashMap<>();
+							HashMap<String, Boolean> enchantmentChecks12 = new HashMap<>();
+							HashMap<Integer, Boolean> enchantmentChecks11 = new HashMap<>();
 							while (sc.hasNext()) {
 								String s1 = sc.next();
 								for (String s : items)
@@ -104,9 +109,17 @@ public class RegistryHelper {
 								for (String s : version_tileEntities)
 									if (!tileEntitiesChecks.containsKey(s) && s1.contains(s))
 										tileEntitiesChecks.put(s, true);
-								for (String s : enchantments)
-									if (!enchantmentChecks.containsKey(s) && s1.contains(s))
-										enchantmentChecks.put(s, true);
+								if (flagLessThan11)
+									for (Integer i : enchantments_11)
+										if (!enchantmentChecks11.containsKey(i) && s1.contains(i.toString()))
+											enchantmentChecks11.put(i, true);
+								else
+									for (String s : enchantments_12)
+										if (!enchantmentChecks12.containsKey(s) && s1.contains(s))
+											enchantmentChecks12.put(s, true);
+							}
+							if (!enchantmentChecks11.isEmpty()) {
+								FlameConfig.field.append("enchant checks: " + enchantmentChecks11 + "\n");
 							}
 							if (blockChecks.size() == (version_blocks.length)) {
 								registries.put("minecraft:blocks", entry.getName());
@@ -120,7 +133,10 @@ public class RegistryHelper {
 							} else if (entityChecks.size() == (version_entities.length)) {
 								registries.put("minecraft:entities", entry.getName());
 								FlameConfig.field.append("entity registry class:" + entry.getName() + "\n");
-							} else if (enchantmentChecks.size() == (enchantments.length)) {
+							} else if (flagLessThan11 && enchantmentChecks11.size() == (enchantments_11.length)) {
+								registries.put("minecraft:enchantments", entry.getName());
+								FlameConfig.field.append("enchantments registry class:" + entry.getName() + "\n");
+							} else if (enchantmentChecks12.size() == (enchantments_12.length)) {
 								registries.put("minecraft:enchantments", entry.getName());
 								FlameConfig.field.append("enchantments registry class:" + entry.getName() + "\n");
 							}
